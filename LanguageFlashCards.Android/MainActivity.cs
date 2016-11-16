8083f9d920b9d035b38cde659f510e02a0c27c8a
alexcartwright1 <alexcartwright@live.com>
2016-11-16 20:44:39 +1000

    Added Android project

---
+++ LanguageFlashCards.Android/MainActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace LanguageFlashCards.Android {
+    [Activity(Label = "LanguageFlashCards.Android", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity {
+        protected override void OnCreate(Bundle bundle) {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
